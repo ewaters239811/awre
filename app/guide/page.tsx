065b203a222b4eec644e4459e78c34be280095e3
Elijah Waters <ewaters2398@gmail.com>
2026-06-18 12:28:27 -0400

    Revert "Make Guide chat immersive"
    This reverts commit c1e67d5d19084ede418123bf76924de6e4800639.

--- app/guide/page.tsx
+++ app/guide/page.tsx
@@ -2,7 +2,7 @@ import { GuideChat } from "@/components/guide-chat";
 
 export default function GuidePage() {
   return (
-    <main className="min-h-screen py-6 md:py-10">
+    <main className="container py-8 md:py-12">
       <GuideChat />
     </main>
   );
